7236396c38898c4938df344a7d48c094a45b3f9e
David T Zirima <52695278+dzzirima@users.noreply.github.com>
2023-08-28 16:13:06 +0200

    authorisation working

--- src/main/java/com/example/auth_final2/config/SecurityConfig.java
+++ src/main/java/com/example/auth_final2/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,8 +18,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration // to indicate that a class is a configuration class
-@EnableWebSecurity // it indicates that the application is using Spring Security for securing the web requests
+@Configuration // to indicate that a class is a configuration class.
+@EnableWebSecurity // it indicates that the application is using Spring Security for securing the web requests.
+@EnableMethodSecurity // allows us to use roles  in our system.
 public class SecurityConfig {
     // used to have 2 overloaded config methods , now we have to do it manually
     // we have to create a bean for each
--- src/main/java/com/example/auth_final2/controllers/MessageController.java
+++ src/main/java/com/example/auth_final2/controllers/MessageController.java
@@ -1,6 +1,7 @@
 package com.example.auth_final2.controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,13 +16,16 @@ public class MessageController {
     }
 
 
+
     @GetMapping("/admin-messages")
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
     public ResponseEntity<List<String>> adminMessage(){
         return  ResponseEntity.ok(Arrays.asList("admin-secret" , "secret-admin"));
     }
 
 
     @GetMapping("/user-messages")
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
     public ResponseEntity<List<String>> userMessage(){
         return  ResponseEntity.ok(Arrays.asList("user-message" , "users dont have secrets"));
     }
